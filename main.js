70256ba35f0e91661d10168a74d7f997391f4092
KritikoIhor <kritikoigor@gmail.com>
2023-06-05 15:49:29 +0300

    chi-wa-u-wa

---
+++ main.js
@@ -0,0 +1,61 @@
+class Human {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+}
+
+class Apartment {
+    residents = [];
+
+    addResident(human) {
+        this.residents.push(human);
+    }
+}
+
+class House {
+    apartments = [];
+
+    constructor(maxApartments) {
+        this.maxApartments = maxApartments;
+    }
+
+    addApartment(apartment) {
+        if(this.apartments.length < this.maxApartments) {
+            this.apartments.push(apartment);
+        } else {
+            console.log('Maximum number of apartments reached!')
+        }
+    }
+}
+
+const personOne = new Human('Andre','man');
+const personTwo = new Human('Emma','woman');
+const personThree = new Human('Ihor','man');
+
+// console.log(personOne);
+// console.log(personTwo);
+// console.log(personThree);
+
+const firstApartment = new Apartment();
+const secondApartment = new Apartment();
+const thirdApartment = new Apartment();
+
+// console.log(firstApartment);
+// console.log(secondApartment);
+
+firstApartment.addResident(personOne);
+secondApartment.addResident(personTwo);
+thirdApartment.addResident(personThree);
+
+const house = new House(2);
+
+house.addApartment(firstApartment);
+house.addApartment(secondApartment);
+house.addApartment(thirdApartment);
+
+// console.log(firstApartment.residents);
+// console.log(secondApartment.residents);
+// console.log(thirdApartment.residents);
+
+console.log(house);
